8204488ab7f1021085f7ade0a9f8691d36f4271c
mohammed younis <mohammedyounis1996@gmail.com>
2023-12-14 14:49:15 -0600

    Data transformation complete

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 #this is a decorator: you can use it when you only are defining variables in the calss,if you have other functions it is not recommended
 
 @dataclass
@@ -47,3 +49,5 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
