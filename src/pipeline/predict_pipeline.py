86e7dd62648beab320fb54f9a8ddd43cdec350bc
mohammed younis <mohammedyounis1996@gmail.com>
2023-12-21 10:04:00 -0600

    fixing predict pipe

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,9 +1,9 @@
-#importing
 import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
 import os
+
 
 class PredictPipeline:
     def __init__(self):
@@ -12,11 +12,9 @@ class PredictPipeline:
     def predict(self,features):
         try:
             model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','proprocessor.pkl')
-            print("Before Loading")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
@@ -25,7 +23,7 @@ class PredictPipeline:
             raise CustomException(e,sys)
 
 
-#customdata class: resposible for mapping all the inputs that we are giving in the html to the backend
+
 class CustomData:
     def __init__(  self,
         gender: str,
